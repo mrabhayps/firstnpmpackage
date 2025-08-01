6b2dc8190a3eb965085f5fdcc2d24e7f0f410c51
mrabhayps <mrabhayps@gmail.com>
2025-08-01 18:26:32 +0530

    common js export

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const msg="This is Simple package for test the concept of creating package and used in another project";
-export function printApplication(name,message=msg){
+
+//ESM export module for import.
+// export function printApplication(name,message=msg){
+//     console.log(`Hello ${name} , Welcome to my simple package test.`);
+//     console.log(message);
+// }
+
+function printApplication(name,message=msg){
     console.log(`Hello ${name} , Welcome to my simple package test.`);
     console.log(message);
+}
+
+//common JS module export 
+
+module.exports={
+    printApplication
 }
